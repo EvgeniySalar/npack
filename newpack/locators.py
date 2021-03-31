fbbe7ec1cd7de2a079d332d268659ce59e8d0462
EvgeniySalar <74773803+EvgeniySalar@users.noreply.github.com>
2021-03-31 10:23:45 +0300

    Delete newpack directory

--- newpack/locators.py
+++
@@ -1,8 +0,0 @@
-GUESTS_COUNT = 'xp__guests__count'
-INCREASE_PERSON_NUMBER = '//button[@aria-label="Increase number of Children"]'
-DATA_GROUP_CHILD_AGE = "//select[@aria-label='Child 1 age']"
-CHILD_AGE_SELECTION = '//select[@aria-label="Child 1 age"]//option[@value="10"][normalize-space()="10 years old"]'
-DATA_GROUP_CHILD_AGE_2 = "//select[@aria-label='Child 2 age']"
-CHILD_AGE_SELECTION_1 = '//select[@aria-label="Child 2 age"]//option[@value="6"][normalize-space()="6 years old"]'
-EMPTY_FIELD = '//div[@class="xpi__searchbox js-ds-layout-events-search-form"]'
-
